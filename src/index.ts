da9774e227d08a0816dd23979e7158d08c9e35ec
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-09-13 18:00:23 +1200

    fix: explicitly specify what should be publicly exposed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export * from "./deepmerge";
-export * from "./types";
-export * from "./utils";
+export { deepmerge, deepmergeCustom } from "./deepmerge";
+
+export type { DeepMergeMergeFunctionsDefaults } from "./deepmerge";
+export type {
+  DeepMergeArraysDefaultHKT,
+  DeepMergeHKT,
+  DeepMergeLeaf,
+  DeepMergeLeafHKT,
+  DeepMergeLeafURI,
+  DeepMergeMapsDefaultHKT,
+  DeepMergeMergeFunctionsDefaultURIs,
+  DeepMergeMergeFunctionsURIs,
+  DeepMergeMergeFunctionURItoKind,
+  DeepMergeMergeFunctionUtils,
+  DeepMergeOptions,
+  DeepMergeRecordsDefaultHKT,
+  DeepMergeSetsDefaultHKT,
+} from "./types";
