67fc4f959ee8fbd62dc2c7dc9bbb423d769d5bce
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-02-18 01:58:55 +1300

    fix: update legacy types

--- types-legacy/v4_0.d.ts
+++ types-legacy/v4_0.d.ts
@@ -5,46 +5,50 @@
  * designed to work with versions of TypeScript < 4.1
  */
 
+declare type MetaDataUpdater<M, MM extends Record<keyof any, any>> = (previousMeta: M | undefined, metaMeta: MM) => M;
+
 /**
  * All the options the user can pass to customize deepmerge.
  */
-declare type DeepMergeOptionsFull = Readonly<{
-  mergeRecords: DeepMergeMergeFunctions["mergeRecords"] | false;
-  mergeArrays: DeepMergeMergeFunctions["mergeArrays"] | false;
-  mergeMaps: DeepMergeMergeFunctions["mergeMaps"] | false;
-  mergeSets: DeepMergeMergeFunctions["mergeSets"] | false;
-  mergeOthers: DeepMergeMergeFunctions["mergeOthers"];
+declare type DeepMergeOptionsFull<M, MM extends Record<keyof any, any>> = Readonly<{
+  mergeRecords: DeepMergeMergeFunctions<M, MM>["mergeRecords"] | false;
+  mergeArrays: DeepMergeMergeFunctions<M, MM>["mergeArrays"] | false;
+  mergeMaps: DeepMergeMergeFunctions<M, MM>["mergeMaps"] | false;
+  mergeSets: DeepMergeMergeFunctions<M, MM>["mergeSets"] | false;
+  mergeOthers: DeepMergeMergeFunctions<M, MM>["mergeOthers"];
+  metaDataUpdater: MetaDataUpdater<M, MM>;
 }>;
 
 /**
  * All the merge functions that deepmerge uses.
  */
-declare type DeepMergeMergeFunctions = Readonly<{
-  mergeRecords: <Ts extends Readonly<ReadonlyArray<Readonly<Record<keyof any, any>>>>, U extends DeepMergeMergeFunctionUtils>(records: Ts, utils: U) => any;
-  mergeArrays: <Ts extends Readonly<ReadonlyArray<Readonly<ReadonlyArray<any>>>>, U extends DeepMergeMergeFunctionUtils>(records: Ts, utils: U) => any;
-  mergeMaps: <Ts extends Readonly<ReadonlyArray<Readonly<ReadonlyMap<any, any>>>>, U extends DeepMergeMergeFunctionUtils>(records: Ts, utils: U) => any;
-  mergeSets: <Ts extends Readonly<ReadonlyArray<Readonly<ReadonlySet<any>>>>, U extends DeepMergeMergeFunctionUtils>(records: Ts, utils: U) => any;
-  mergeOthers: <Ts extends Readonly<ReadonlyArray<any>>, U extends DeepMergeMergeFunctionUtils>(records: Ts, utils: U) => any;
+declare type DeepMergeMergeFunctions<M, MM extends Record<keyof any, any>> = Readonly<{
+  mergeRecords: <Ts extends ReadonlyArray<Readonly<Record<keyof any, any>>>, U extends DeepMergeMergeFunctionUtils<M, MM>>(records: Ts, utils: U, meta: M | undefined) => any;
+  mergeArrays: <Ts extends ReadonlyArray<ReadonlyArray<any>>, U extends DeepMergeMergeFunctionUtils<M, MM>>(records: Ts, utils: U, meta: M | undefined) => any;
+  mergeMaps: <Ts extends ReadonlyArray<Readonly<ReadonlyMap<any, any>>>, U extends DeepMergeMergeFunctionUtils<M, MM>>(records: Ts, utils: U, meta: M | undefined) => any;
+  mergeSets: <Ts extends ReadonlyArray<Readonly<ReadonlySet<any>>>, U extends DeepMergeMergeFunctionUtils<M, MM>>(records: Ts, utils: U, meta: M | undefined) => any;
+  mergeOthers: <Ts extends ReadonlyArray<any>, U extends DeepMergeMergeFunctionUtils<M, MM>>(records: Ts, utils: U, meta: M | undefined) => any;
 }>;
 
 /**
  * The utils provided to the merge functions.
  */
-declare type DeepMergeMergeFunctionUtils = Readonly<{
-  mergeFunctions: DeepMergeMergeFunctions;
-  defaultMergeFunctions: DeepMergeMergeFunctionsDefaults;
-  deepmerge: <Ts extends Readonly<ReadonlyArray<any>>>(...values: Ts) => any;
+declare type DeepMergeMergeFunctionUtils<M, MM extends Record<keyof any, any>> = Readonly<{
+  mergeFunctions: DeepMergeMergeFunctions<M, MM>;
+  defaultMergeFunctions: DeepMergeMergeFunctionsDefaults<M, MM>;
+  metaDataUpdater: MetaDataUpdater<M, MM>;
+  deepmerge: <Ts extends ReadonlyArray<any>>(...values: Ts) => any;
 }>;
 
 /**
  * The default merge functions.
  */
-declare type DeepMergeMergeFunctionsDefaults = Readonly<{
-  mergeMaps: (values: Record<keyof any, any>[], utils: DeepMergeMergeFunctionUtils) => any;
-  mergeSets: (values: any[][], utils: DeepMergeMergeFunctionUtils) => any;
-  mergeArrays: (values: Set<any>[], utils: DeepMergeMergeFunctionUtils) => any;
-  mergeRecords: (values: Map<any, any>[], utils: DeepMergeMergeFunctionUtils) => any;
-  mergeOthers: (values: any[], utils: DeepMergeMergeFunctionUtils) => any;
+declare type DeepMergeMergeFunctionsDefaults<M, MM extends Record<keyof any, any>> = Readonly<{
+  mergeMaps: (values: Record<keyof any, any>[]) => any;
+  mergeSets: (values: any[][]) => any;
+  mergeArrays: (values: Set<any>[]) => any;
+  mergeRecords: (values: Map<any, any>[], utils: DeepMergeMergeFunctionUtils<M, MM>, meta: M | undefined) => any;
+  mergeOthers: (values: any[]) => any;
 }>;
 
 /**
@@ -66,10 +70,11 @@ declare function deepmerge<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9>(arg0: T0, arg
 declare function deepmerge(...args: any[]): any;
 
 /**
- * Deeply merge two or more objects using the given options.
+ * Deeply merge two or more objects using the given options and meta data.
  *
  * @param options - The options on how to customize the merge function.
+ * @param rootMetaData - The meta data passed to the root items' being merged.
  */
-declare function deepmergeCustom(options: Partial<DeepMergeOptionsFull>): (...objects: any[]) => any;
+declare function deepmergeCustom(options: Partial<DeepMergeOptionsFull<any, any>>, rootMetaData?: any): (...objects: any[]) => any;
 
 export { deepmerge, deepmergeCustom };
