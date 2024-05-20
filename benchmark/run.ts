b40278fd516319b9817dbfe57ce5800ae5eb2d35
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2024-05-20 21:31:59 +1200

    chore: update benchmarks

--- benchmark/run.ts
+++ benchmark/run.ts
@@ -1,34 +1,51 @@
-import { promises as fs, constants as fsConstants } from "node:fs";
+/* eslint-disable no-await-in-loop */
+import * as fs from "node:fs";
+import * as fsp from "node:fs/promises";
 import * as path from "node:path";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
-import Benchmark from "benchmark";
-import { all as deepmerge } from "deepmerge";
+import deepmerge from "deepmerge";
 import { deepmerge as deepmergeTs } from "deepmerge-ts";
-import { merge as lodashMerge } from "lodash";
+import lodash from "lodash";
 import { merge as mergeAnything } from "merge-anything";
 import { Accumulator as ObjectAccumulator } from "object-accumulator";
+import { Bench } from "tinybench";
 
-const benchmarkDataFile = path.join(__dirname, "data.json");
+const benchmarkDataFile = path.join(
+  dirname(fileURLToPath(import.meta.url)),
+  "data.json",
+);
 
-const benchmarkDataSets: any[][] = await fs
-  .access(benchmarkDataFile, fsConstants.R_OK)
+const benchmarkDataSets: Array<{
+  name: string;
+  data: object[];
+}> = await fsp
+  .access(benchmarkDataFile, fs.constants.R_OK)
   .then(async () => {
     console.log("Loading benchmark data file.");
-    const data = await fs.readFile(benchmarkDataFile, { encoding: "utf8" });
+    const data = await fsp.readFile(benchmarkDataFile, { encoding: "utf8" });
     return JSON.parse(data);
   })
   .catch(async (error: unknown) => {
-    if (error?.code !== "ENOENT") {
+    if (
+      !(error instanceof Error) ||
+      !("code" in error) ||
+      error.code !== "ENOENT"
+    ) {
       throw error;
     }
-    console.log("No benchmark data file found. Generating benchmark data.");
+    console.log(
+      "No benchmark data file found. Generating random data for benchmarking against.",
+    );
 
     const data = [
-      generateBenchmarkDataArray(2, 4, 15),
-      generateBenchmarkDataArray(100, 10, 4),
+      generateBenchmarkDataSet("tall", 2, 3, 16),
+      generateBenchmarkDataSet("wide", 100, 12, 4),
+      generateBenchmarkDataSet("mid", 10, 6, 8),
     ];
 
-    await fs.writeFile(benchmarkDataFile, JSON.stringify(data), {
+    await fsp.writeFile(benchmarkDataFile, JSON.stringify(data), {
       encoding: "utf8",
     });
 
@@ -36,22 +53,26 @@ const benchmarkDataSets: any[][] = await fs
   });
 
 for (let m_i = 0; m_i < benchmarkDataSets.length; m_i++) {
-  const benchmarkData = benchmarkDataSets[m_i];
-  const suite = new Benchmark.Suite();
+  const { name: benchmarkName, data: benchmarkData } = benchmarkDataSets[m_i];
+  const bench = new Bench({
+    time: 10_000,
+    iterations: 1,
+    warmupTime: 1000,
+    warmupIterations: 1,
+  });
 
   console.log(
-    `\nRunning benchmarks for data set ${m_i + 1} of ${
+    `\nRunning benchmarks for data set "${benchmarkName}" (${m_i + 1} of ${
       benchmarkDataSets.length
-    }:\n`,
+    }):\n`,
   );
 
-  // add tests
-  suite
+  bench
     .add("deepmerge-ts", () => {
       deepmergeTs(...benchmarkData);
     })
     .add("deepmerge", () => {
-      deepmerge(benchmarkData);
+      deepmerge.all(benchmarkData);
     })
     .add("merge-anything", () => {
       (mergeAnything as any)(...benchmarkData);
@@ -59,39 +80,40 @@ for (let m_i = 0; m_i < benchmarkDataSets.length; m_i++) {
     .add("object-accumulator", () => {
       ObjectAccumulator.from(benchmarkData).merge();
     })
-    .add("lowdash merge", () => {
-      lodashMerge({}, benchmarkData);
-    })
-    .on("cycle", (event: any) => {
-      console.log(String(event.target));
-    })
-    // eslint-disable-next-line func-names
-    .on("complete", function () {
-      // @ts-expect-error When need to access the "this" value
-      // eslint-disable-next-line unicorn/no-this-assignment, ts/no-this-alias
-      const results = this;
+    .add("lodash merge", () => {
+      lodash.merge({}, benchmarkData);
+    });
 
-      console.log(
-        `\nFastest is ${results.filter("fastest").map("name")} for data set ${
-          m_i + 1
-        } of ${benchmarkDataSets.length}`,
-      );
-    })
-    .run({ async: false });
+  await bench.warmup();
+  await bench.run();
+
+  console.table(bench.table());
 }
 
-function generateBenchmarkDataArray(items, maxProperties, maxDepth) {
+function generateBenchmarkDataSet(
+  name: string,
+  items: number,
+  maxProperties: number,
+  maxDepth: number,
+) {
   const data: object[] = [];
 
   for (let m_i = 0; m_i < items; m_i++) {
     data.push(generateBenchmarkDataItem(maxProperties, maxDepth));
   }
 
-  return data;
+  return {
+    name,
+    data,
+  };
 }
 
-function generateBenchmarkDataItem(maxProperties, depth, currentDepth = 0) {
-  const obj = {};
+function generateBenchmarkDataItem(
+  maxProperties: number,
+  depth: number,
+  currentDepth = 0,
+) {
+  const obj: object = {};
 
   const properties = Math.floor(maxProperties * Math.random()) + 1;
 
@@ -113,7 +135,7 @@ function generateBenchmarkDataItem(maxProperties, depth, currentDepth = 0) {
   return obj;
 }
 
-function shuffle(array) {
+function shuffle<T>(array: T[]) {
   let m_currentIndex = array.length;
   let m_randomIndex;
 
